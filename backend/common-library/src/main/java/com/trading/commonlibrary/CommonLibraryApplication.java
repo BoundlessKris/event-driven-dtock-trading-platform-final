b6842534bfe41a7e76706c3a80874fe13e28b399
Boundless_Coder <coderankit2452@gmail.com>
2025-03-25 03:07:34 +0530

    setting up the things for working(setting up basic interactions)

---
+++ backend/common-library/src/main/java/com/trading/commonlibrary/CommonLibraryApplication.java
@@ -0,0 +1,9 @@
+package com.trading.commonlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class CommonLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLibraryApplication.class, args);
+    }
+}
---
+++ backend/portfolio-service/src/main/java/com/trading/portfolioservice/PortfolioServiceApplication.java
@@ -0,0 +1,9 @@
+package com.trading.portfolioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class PortfolioServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioServiceApplication.class, args);
+    }
+}
